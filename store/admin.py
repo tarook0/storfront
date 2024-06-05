6f5e193931b721ffb434b9d0a6bd82ad173de81c
tarook331@gmail.com <tarook331@gmail.com>
2024-06-05 08:40:34 +0300

    add model class to admin app and do full seed.

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,44 @@
+from itertools import count
+from typing import Counter
 from django.contrib import admin
+from . import models
+
+@admin.register(models.Product)  # with this we no more need for this admin.site.register(models.Product,ProductAdmin)
+class ProductAdmin(admin.ModelAdmin):
+    list_display=['title','unit_price','invenory_status','collection_title']
+    list_editable=['unit_price']
+    list_per_page=10
+    list_select_related=['collection']
+    def collection_title(self,product):
+        return product.collection.title 
+    @admin.display(ordering='inventory')
+    def invenory_status(self,product):
+        if product.inventory<10:
+            return 'Low'
+        return 'Ok'
+    # for list of this google for modellist admin 
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display=['title','products_count']
+    
+    def products_count(self,collection):
+        return collection.products_count
+    
+    def get_queryset(self,request):
+        return  super().get_queryset(request).annotate(
+            products_count=count('product')
+        )
+
+# admin.site.register(models.Product,ProductAdmin)
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display=['first_name','last_name','membership']
+    list_editable=['membership']
+    ordering=['first_name','last_name']
+    list_per_page=10
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display=['id','placed_at','customer']
 
 # Register your models here.
--- store/models.py
+++ store/models.py
@@ -4,7 +4,11 @@ from django.db import models
 class Collection(models.Model):
     title = models.CharField(max_length=255)
     featured_products=models.ForeignKey('Product',on_delete=models.SET_NULL,null=True,related_name='+')
-
+    def __str__(self) ->str:
+        return self.title
+    class Meta:
+        ordering=['title']
+    
 class Promotion(models.Model):
     description=models.CharField(max_length=255)
     discount=models.FloatField()
@@ -18,6 +22,11 @@ class Product(models.Model):
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotions=models.ManyToManyField(Promotion)
+    def __str__(self)->str:
+        return self.title
+    class Meta:
+        ordering=['title']
+    
 
 class Item(models.Model):
     name = models.CharField(max_length=255)
@@ -40,6 +49,13 @@ class Customer(models.Model):
     birth_date = models.DateField(null=True)
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBER_BORANZ)
+    def __str__(self):
+        return f'{self.first_name}{self.last_name}'
+    class Meta:
+        ordering=['first_name','last_name']
+    
+    
+    
     # class Meta:
     #     db_table='store_customers'
     #     indexes=[models.Index(fields=['last_name','first_name'])]
@@ -53,10 +69,15 @@ class Order(models.Model):
         (PAYMENT_STATUS_COMPLETE, 'Complete'),
         (PAYMENT_STATUS_FAILED, 'Faild'),
     ]
-    palced_at = models.DateTimeField(auto_now_add=True)
+    placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(
         max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    # def __str__(self)->str:
+    #     return self.id
+    # class Meta:
+    #     Ordering=['id']
+    
 
 
 class OrderItem(models.Model):
